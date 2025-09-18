6ed7e0ff9579961ae3cf6e331b73f2351968fcd0
jeancopzm <jeancopzm@gmail.com>
2025-09-18 12:27:24 +0000

    Initial commit

---
+++ .open-next/server-functions/default/.next/server/chunks/324.js
@@ -0,0 +1,205 @@
+"use strict";exports.id=324,exports.ids=[324],exports.modules={8911:(s,e,i)=>{i.d(e,{EC:()=>C,mK:()=>w});var l,t,n,S,_,d,A,a,c,o,r,u,y,C,h,D,P=i(8825);(function(s){s.ContentTypeAsc="contentType_ASC",s.ContentTypeDesc="contentType_DESC",s.FileNameAsc="fileName_ASC",s.FileNameDesc="fileName_DESC",s.HeightAsc="height_ASC",s.HeightDesc="height_DESC",s.SizeAsc="size_ASC",s.SizeDesc="size_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC",s.UrlAsc="url_ASC",s.UrlDesc="url_DESC",s.WidthAsc="width_ASC",s.WidthDesc="width_DESC"})(l||(l={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.PublishedDateAsc="publishedDate_ASC",s.PublishedDateDesc="publishedDate_DESC",s.SlugAsc="slug_ASC",s.SlugDesc="slug_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC",s.TitleAsc="title_ASC",s.TitleDesc="title_DESC"}(t||(t={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.NameAsc="name_ASC",s.NameDesc="name_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC"}(n||(n={})),function(s){s.CaptionAsc="caption_ASC",s.CaptionDesc="caption_DESC",s.FullWidthAsc="fullWidth_ASC",s.FullWidthDesc="fullWidth_DESC",s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC"}(S||(S={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.PublishedDateAsc="publishedDate_ASC",s.PublishedDateDesc="publishedDate_DESC",s.SlugAsc="slug_ASC",s.SlugDesc="slug_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC",s.TitleAsc="title_ASC",s.TitleDesc="title_DESC"}(_||(_={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC"}(d||(d={})),function(s){s.CanonicalUrlAsc="canonicalUrl_ASC",s.CanonicalUrlDesc="canonicalUrl_DESC",s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.NofollowAsc="nofollow_ASC",s.NofollowDesc="nofollow_DESC",s.NoindexAsc="noindex_ASC",s.NoindexDesc="noindex_DESC",s.PageTitleAsc="pageTitle_ASC",s.PageTitleDesc="pageTitle_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC"}(A||(A={})),function(s){s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC"}(a||(a={})),function(s){s.Avif="AVIF",s.Jpg="JPG",s.JpgProgressive="JPG_PROGRESSIVE",s.Png="PNG",s.Png8="PNG8",s.Webp="WEBP"}(c||(c={})),function(s){s.Bottom="BOTTOM",s.BottomLeft="BOTTOM_LEFT",s.BottomRight="BOTTOM_RIGHT",s.Center="CENTER",s.Face="FACE",s.Faces="FACES",s.Left="LEFT",s.Right="RIGHT",s.Top="TOP",s.TopLeft="TOP_LEFT",s.TopRight="TOP_RIGHT"}(o||(o={})),function(s){s.Crop="CROP",s.Fill="FILL",s.Fit="FIT",s.Pad="PAD",s.Scale="SCALE",s.Thumb="THUMB"}(r||(r={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.PublishedDateAsc="publishedDate_ASC",s.PublishedDateDesc="publishedDate_DESC",s.SlugAsc="slug_ASC",s.SlugDesc="slug_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC",s.TitleAsc="title_ASC",s.TitleDesc="title_DESC"}(u||(u={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC"}(y||(y={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.PublishedDateAsc="publishedDate_ASC",s.PublishedDateDesc="publishedDate_DESC",s.SlugAsc="slug_ASC",s.SlugDesc="slug_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC",s.TitleAsc="title_ASC",s.TitleDesc="title_DESC"}(C||(C={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.PublishedDateAsc="publishedDate_ASC",s.PublishedDateDesc="publishedDate_DESC",s.SlugAsc="slug_ASC",s.SlugDesc="slug_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC",s.TitleAsc="title_ASC",s.TitleDesc="title_DESC"}(h||(h={})),function(s){s.InternalNameAsc="internalName_ASC",s.InternalNameDesc="internalName_DESC",s.SysFirstPublishedAtAsc="sys_firstPublishedAt_ASC",s.SysFirstPublishedAtDesc="sys_firstPublishedAt_DESC",s.SysIdAsc="sys_id_ASC",s.SysIdDesc="sys_id_DESC",s.SysPublishedAtAsc="sys_publishedAt_ASC",s.SysPublishedAtDesc="sys_publishedAt_DESC",s.SysPublishedVersionAsc="sys_publishedVersion_ASC",s.SysPublishedVersionDesc="sys_publishedVersion_DESC"}(D||(D={}));let b=(0,P.ZP)`
+    fragment ImageFields on Asset {
+  __typename
+  sys {
+    id
+  }
+  title
+  description
+  width
+  height
+  url
+  contentType
+}
+    `,p=(0,P.ZP)`
+    fragment SeoFields on ComponentSeo {
+  __typename
+  pageTitle
+  pageDescription
+  canonicalUrl
+  nofollow
+  noindex
+  shareImagesCollection(limit: 3, locale: $locale) {
+    items {
+      ...ImageFields
+    }
+  }
+}
+    `,g=(0,P.ZP)`
+    fragment AuthorFields on ComponentAuthor {
+  __typename
+  sys {
+    id
+  }
+  name
+  avatar {
+    ...ImageFields
+  }
+}
+    `,E=(0,P.ZP)`
+    fragment RichImageFields on ComponentRichImage {
+  __typename
+  internalName
+  sys {
+    id
+  }
+  image {
+    ...ImageFields
+  }
+  caption
+  fullWidth
+}
+    `,m=(0,P.ZP)`
+    fragment ReferencePageBlogPostFields on PageBlogPost {
+  __typename
+  sys {
+    id
+    spaceId
+  }
+  slug
+  author {
+    ...AuthorFields
+  }
+  publishedDate
+  title
+  shortDescription
+  featuredImage {
+    ...ImageFields
+  }
+}
+    `,N=(0,P.ZP)`
+    fragment PageBlogPostFields on PageBlogPost {
+  __typename
+  sys {
+    id
+    spaceId
+  }
+  internalName
+  seoFields {
+    ...SeoFields
+  }
+  slug
+  author {
+    ...AuthorFields
+  }
+  publishedDate
+  title
+  shortDescription
+  featuredImage {
+    ...ImageFields
+  }
+  content {
+    json
+    links {
+      entries {
+        block {
+          ...RichImageFields
+        }
+      }
+    }
+  }
+  relatedBlogPostsCollection(limit: 2) {
+    items {
+      ...ReferencePageBlogPostFields
+    }
+  }
+}
+    `,F=(0,P.ZP)`
+    fragment PageLandingFields on PageLanding {
+  __typename
+  sys {
+    id
+    spaceId
+  }
+  internalName
+  seoFields {
+    ...SeoFields
+  }
+  featuredBlogPost {
+    ...ReferencePageBlogPostFields
+  }
+}
+    `,I=(0,P.ZP)`
+    fragment sitemapPagesFields on Query {
+  pageBlogPostCollection(limit: 100, locale: $locale) {
+    items {
+      slug
+      sys {
+        publishedAt
+      }
+    }
+  }
+  pageLandingCollection(limit: 1, locale: $locale) {
+    items {
+      sys {
+        publishedAt
+      }
+    }
+  }
+}
+    `,f=(0,P.ZP)`
+    query pageBlogPost($slug: String!, $locale: String, $preview: Boolean) {
+  pageBlogPostCollection(
+    limit: 1
+    where: {slug: $slug}
+    locale: $locale
+    preview: $preview
+  ) {
+    items {
+      ...PageBlogPostFields
+    }
+  }
+}
+    ${N}
+${p}
+${b}
+${g}
+${E}
+${m}`,T=(0,P.ZP)`
+    query pageBlogPostCollection($locale: String, $preview: Boolean, $limit: Int, $order: [PageBlogPostOrder], $where: PageBlogPostFilter) {
+  pageBlogPostCollection(
+    limit: $limit
+    locale: $locale
+    preview: $preview
+    order: $order
+    where: $where
+  ) {
+    items {
+      ...PageBlogPostFields
+    }
+  }
+}
+    ${N}
+${p}
+${b}
+${g}
+${E}
+${m}`,V=(0,P.ZP)`
+    query pageLanding($locale: String, $preview: Boolean) {
+  pageLandingCollection(limit: 1, locale: $locale, preview: $preview) {
+    items {
+      ...PageLandingFields
+    }
+  }
+}
+    ${F}
+${p}
+${b}
+${m}
+${g}`,$=(0,P.ZP)`
+    query pageLandingCollection($locale: String, $preview: Boolean) {
+  pageLandingCollection(limit: 100, locale: $locale, preview: $preview) {
+    items {
+      ...PageLandingFields
+    }
+  }
+}
+    ${F}
+${p}
+${b}
+${m}
+${g}`,B=(0,P.ZP)`
+    query sitemapPages($locale: String!) {
+  ...sitemapPagesFields
+}
+    ${I}`,L=(s,e,i,l)=>s();function w(s,e=L){return{pageBlogPost:(i,l)=>e(e=>s.request(f,i,{...l,...e}),"pageBlogPost","query",i),pageBlogPostCollection:(i,l)=>e(e=>s.request(T,i,{...l,...e}),"pageBlogPostCollection","query",i),pageLanding:(i,l)=>e(e=>s.request(V,i,{...l,...e}),"pageLanding","query",i),pageLandingCollection:(i,l)=>e(e=>s.request($,i,{...l,...e}),"pageLandingCollection","query",i),sitemapPages:(i,l)=>e(e=>s.request(B,i,{...l,...e}),"sitemapPages","query",i)}}},9324:(s,e,i)=>{i.d(e,{L:()=>A,S:()=>a});var l=i(6649),t=i(8911);let n=process.env.CONTENTFUL_GRAPHQL_ENDPOINT,S=`${n||"https://graphql.contentful.com/content/v1/spaces"}/${process.env.CONTENTFUL_SPACE_ID}/environments/${process.env.CONTENTFUL_SPACE_ENVIRONMENT||"master"}`;process.env.CONTENTFUL_ACCESS_TOKEN;let _=new l.g6(S,{headers:{Authorization:`Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`}}),d=new l.g6(S,{headers:{Authorization:`Bearer ${process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN}`}}),A=(0,t.mK)(_),a=(0,t.mK)(d)}};
